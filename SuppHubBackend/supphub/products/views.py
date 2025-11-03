72d626072f1564fc947c06676a6747ae796c17e6
kak!us56 <rudniyyarik@gmail.com>
2025-11-03 14:06:12 +0100

    add import-products

--- SuppHubBackend/supphub/products/views.py
+++ SuppHubBackend/supphub/products/views.py
@@ -1,12 +1,15 @@
+import csv
+import io
+from django.shortcuts import redirect
 from rest_framework import routers, serializers, viewsets
+from rest_framework.decorators import permission_classes
 
 from .models import Product
 
-# from rest_framework.request import Request
-# from rest_framework.response import Response
+from rest_framework.request import Request
+from rest_framework.response import Response
 
 
-# Create your views here.
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
@@ -16,6 +19,35 @@ class ProductSerializer(serializers.ModelSerializer):
 class ProductAPIViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+
+def import_products(request):
+    if not request.user.is_superuser:
+        return Response({"detail": "You do not have permission to perform this action."}, status=403)
+
+    if request.method != "POST":
+        raise ValueError("Only POST requests are allowed.")
+
+    csv_file = request.FILES.get("file")
+    if csv_file is None:
+        raise ValueError("CSV file not found.")
+
+    decoded = csv_file.read().decode("utf-8")
+    reader = csv.DictReader(io.StringIO(decoded))
+    total = 0
+
+    for row in reader:
+        try:
+            Product.objects.create(name=row["name"], price=int(row["price"]),
+                hitBool=int(row["hitBool"]), veganBool=int(row["veganBool"]), img=row["img"])
+        except:
+            raise ValueError(f"Error processing row: {row}")
+        
+        total += 1
+
+    print(f"{total} products uploaded to the database.")
+
+    return redirect(request.META.get("HTTP_REFERER", "/"))
 
 
 router = routers.DefaultRouter()
--- SuppHubBackend/supphub/supphub/urls.py
+++ SuppHubBackend/supphub/supphub/urls.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 from django.urls import include, path
 
 from products.views import router as products_router
+from products.views import import_products
 from tgbot.views import submit_order
 
 urlpatterns = [
+    path("admin/products/product/import-products/", import_products, name="import_products"),
     path("admin/", admin.site.urls),
     path("", include(products_router.urls)),
     path("submit/", submit_order, name="submit_order"),
