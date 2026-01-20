58db81e19fd50a416de9b69357f354b4cf2d6e53
kak!us56 <rudniyyarik@gmail.com>
2026-01-20 12:51:42 +0100

    add mobile about page

--- frontend/components/main-about/main-about.tsx
+++ frontend/components/main-about/main-about.tsx
@@ -10,28 +10,28 @@ export const MainAbout = () => {
     return (
         <main>
             <div className="min-h-[1216px] bg-[#1F1F1F] mt-20 max-md:mt-[48px] bg-repeat bg-pill bg-[length:250px] max-md:bg-[length:130px]">
-                <div className="max-w-[1312px] max-md:max-w-[352px] mx-auto px-4 flex flex-col items-center">
-                    <h1 className={`${handjet.className} text-[4rem] leading-cssnormal text-[#D9D9D9] tracking-[5.12px] pt-[53px] pl-[32px] font-medium self-start`}>
+                <div className="max-w-[1312px] max-md:max-w-[382px] mx-auto px-4 flex flex-col items-center">
+                    <h1 className={`${handjet.className} max-md:text-[2rem] text-[4rem] leading-cssnormal text-[#D9D9D9] tracking-[5.12px] pt-[53px] max-md:pt-[48px] pl-[32px] max-md:pl-5 font-medium self-start`}>
                         Про нас
                     </h1>
-                    <div className="w-full bg-[rgba(0,_0,_0,_0.35)] rounded-[24px] px-[50px] pt-[28px] pb-[40px] mt-[22px]">
-                        <section className="text-[1.625rem]/[2.25rem] font-normal text-white">
+                    <div className="w-full bg-[rgba(0,_0,_0,_0.35)] max-md:flex max-md:flex-col max-md:gap-4 max-md:bg-transparent max-md:p-0 rounded-[24px] px-[50px] pt-[28px] pb-[40px] mt-[22px]">
+                        <section className="text-[1.625rem]/[2.25rem] max-md:text-[1rem]/[1.125rem] max-md:p-4 text-justify font-normal text-white max-md:bg-[#343434] max-md:rounded-[18px] max-md:w-full">
                             <h3 className="font-bold">Ми — SuppHub.</h3>
-                            <p className="mb-8">
+                            <p className="mb-8 max-md:mb-4">
                                 Ми не просто інтернет-магазин вітамінів, ми твій компаньйон у світі дедлайнів, інтенсивного навчання та великих амбіцій.
                             </p>
-                            <ol className="list-decimal list-inside mb-8">
-                                <li>Наша мета — допомогти тобі бути продуктивним, бадьорим і не вигоріти.</li>
-                                <li>Наша місія — розблокувати твій потенціал.</li>
+                            <ol className="list-decimal list-inside mb-8 max-md:mb-4 marker:font-bold">
+                                <li><strong>Наша мета</strong> — допомогти тобі бути продуктивним, бадьорим і не вигоріти.</li>
+                                <li><strong>Наша місія</strong> — розблокувати твій потенціал.</li>
                             </ol>
-                            <p className="mb-6">
+                            <p className="mb-8 max-md:m-0">
                                 Ми підбираємо амінокислоти, ноотропи, нутрицевтики та вітамінні комплекси, які реально працюють. Наша продуктова лінійка створена для покращення пам’яті, фокусу, енергії без «відкату» та підтримки імунітету.
                             </p>
                         </section>
-                        <section className="flex justify-between mb-8">
-                            <div className="text-[1.625rem]/[2.25rem] font-normal text-white w-[50%] border-r-[1px] border-r-white pr-4">
+                        <section className="flex justify-between mb-8 max-md:m-0 max-md:flex-col max-md:gap-4">
+                            <div className="text-[1.625rem]/[2.25rem] max-md:text-[1rem]/[1.125rem] max-md:w-full max-md:p-4 max-md:bg-[#494949] max-md:rounded-[18px] max-md:border-none font-normal text-white w-[50%] border-r-[1px] border-r-white pr-4">
                                 <h4 className="font-bold">Що ми пропонуємо</h4>
-                                <ul className="list-inside list-disc marker:text-[0.7em] pl-4 flex flex-col justify-between h-[calc(100%_-_36px)]">
+                                <ul className="list-inside list-disc marker:text-[0.7em] pl-4 max-md:pl-2 flex flex-col justify-between h-[calc(100%_-_36px)]">
                                     <li>Ноотропи для фокусу та пам’яті.</li>
                                     <li>Амінокислоти для відновлення та мотивації.</li>
                                     <li>Енергетичні добавки без «провалів» після дії.</li>
@@ -39,9 +39,9 @@ export const MainAbout = () => {
                                     <li>Вітаміни для підтримки імунітету.</li>
                                 </ul>
                             </div>
-                            <div className="text-[1.625rem]/[2.25rem] font-normal text-white w-[50%] pl-8">
+                            <div className="text-[1.625rem]/[2.25rem] font-normal text-white w-[50%] pl-8 max-md:text-[1rem]/[1.125rem] max-md:w-full max-md:p-4 max-md:bg-[#343434] max-md:rounded-[18px]">
                                 <h4 className="font-bold">Чому обирають SuppHub</h4>
-                                <ul className="list-inside list-disc marker:text-[0.7em] pl-4 flex flex-col justify-between h-[calc(100%_-_36px)]">
+                                <ul className="list-inside list-disc marker:text-[0.7em] pl-4 max-md:pl-2 flex flex-col justify-between h-[calc(100%_-_36px)]">
                                     <li>Біодоступні форми та підтверджені компоненти.</li>
                                     <li>Науковий підхід без обіцянок «магії».</li>
                                     <li>Підходить для студентів, спортсменів і тих, хто поєднує кар’єру з активним життям.</li>
@@ -50,17 +50,17 @@ export const MainAbout = () => {
                                 </ul>
                             </div>
                         </section>
-                        <section>
-                            <div className="text-[1.625rem]/[2.25rem] font-normal text-white mb-8">
+                        <section className="max-md:bg-[#494949] max-md:rounded-[18px] max-md:p-4">
+                            <div className="text-[1.625rem]/[2.25rem] font-normal text-white mb-8 max-md:mb-4 max-md:text-[1rem]/[1.125rem]">
                                 <h4 className="font-bold">SuppHub  для тих, хто:</h4>
-                                <ul className="list-inside list-disc marker:text-[0.7em] pl-4">
+                                <ul className="list-inside list-disc marker:text-[0.7em] pl-4 max-md:pl-2">
                                     <li>готується до іспитів або важливого захисту;</li>
                                     <li>поєднує роботу з тренуваннями;</li>
                                     <li>хоче відчувати бадьорість та швидкість мислення цілодобово;</li>
                                     <li>цінує якість і науковий підхід до свого здоров’я.</li>
                                 </ul>
                             </div>
-                            <div className="text-[1.625rem]/[2.25rem] font-normal text-white mb-8">
+                            <div className="text-[1.625rem]/[2.25rem] font-normal text-white mb-8 max-md:mb-4 max-md:text-[1rem]/[1.125rem]">
                                 <h4 className="font-bold">Наш підхід</h4>
                                 <p>
                                     Кожен продукт у каталозі - результат відбору по критеріях якості, безпеки та ефективності.<br />
@@ -68,7 +68,7 @@ export const MainAbout = () => {
                                     Ми працюємо з перевіреними постачальниками й обираємо форми з високою біодоступністю.
                                 </p>
                             </div>
-                            <div className="text-[1.625rem]/[2.25rem] font-normal text-white mb-8">
+                            <div className="text-[1.625rem]/[2.25rem] font-normal text-white mb-8 max-md:m-0 max-md:text-[1rem]/[1.125rem]">
                                 <h4 className="font-bold">Готовий увімкнути режим суперсили?</h4>
                                 <p>
                                     Почни з нашого бестселера або проконсультуйся з нами — підберемо добавку під твої цілі. <br />
@@ -78,11 +78,11 @@ export const MainAbout = () => {
                         </section>
                     </div>
                     <Link href={"/"}>
-                        <button className="w-[15.375rem] h-[3.25rem] bg-[#F90] rounded-[4px] text-center text-black text-[20px] leading-cssnormal tracking-[1.6px] font-medium mt-[34px]">
+                        <button className="w-[12.375rem] h-[2.9rem] bg-[#F90] rounded-[4px] text-center text-black text-[1.25rem] max-md:text-[1.125rem] leading-cssnormal tracking-[1.6px] font-medium mt-[34px] max-md:mt-[24px]">
                             До каталогу
                         </button>                    
                     </Link>
-                    <div className="pt-8 pb-9">
+                    <div className="pt-8 pb-9 max-md:w-[60px] max-md:pt-5 max-md:pb-6">
                         <Image src="/logo-img.png" alt="Логотип" width={82} height={82} />
                     </div>
                 </div>
